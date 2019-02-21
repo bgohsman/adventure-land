e92a59a8f72a9463dc198b620c81abce8462ae43
Brandon Gohsman <brandon@echoboxinteractive.com>
2019-02-21 08:52:37 -0500

    Add `applyToParty()` routine - `party-members.js`

---
+++ party-members.js
@@ -0,0 +1,22 @@
+function applyToParty() {
+  // TODO:
+  // - Update to use `parent.party_list` instead of hard-coded values
+  const partyMembers = ['Player1', 'Player2', 'Etc'];
+
+  for (let i = 0; i < partyMembers.length; i++) {
+    // Select ability to apply to members...
+    // Only apply if that member meets certain criteria...
+    if (can_use('energize') && character.mp > 1000) {
+      const partyMember = (get_player(partyMembers[i]));
+
+      if (partyMember) {
+        // If the member's selected attribute meets the criteria...
+        if (partyMember.mp < partyMember.max_mp - 310 && in_attack_range(partyMember)) {
+          use_skill('energize', partyMember);
+
+          return;
+        }
+      }
+    }
+  }
+}
